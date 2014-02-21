8e989285ef8bb12885e4a3beeb1d38d142dde766
Wilmer Paulino <wpaulino1115@gmail.com>
2014-02-21 02:37:29 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PracticeApp::Application.config.secret_key_base = '22d746cc67d7918c302d0fc6e945637d925275aa4e32da40ab3b20f1bfe5dbe9fae6f2cb78d909652da50af4a2ad3a1f0f0efc9d995bbc92a8ffb2ef0700ce73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PracticeApp::Application.config.session_store :cookie_store, key: '_practice_app_session'
